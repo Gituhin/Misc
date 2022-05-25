4f02b8b83b4a4c6199962509e5e9cdf40fb14357
Tuhin Subhra De <tuhinsubhrade069@gmail.com>
2022-05-25 19:12:48 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.header("This is a test app deployed using streamlit-io.")
-st.subheader("To display the contents of uploaded files.")
-if st.button("Click me"):
-    st.success("Thanks for clicking")
-
-st.text("That's all, © Tuhin 2022")
